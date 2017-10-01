f4eb94e0b61825781d0deef7adb0d641c7746ced
Koen Klinkers <koen.klinkers@devmob.com>
2017-10-01 15:44:48 +0200

    setup

---
+++ pin_events.py
@@ -0,0 +1,46 @@
+from gpiozero import Button
+from signal import pause
+
+GPIO_PLAY = Button(3)
+GPIO_PLAY.held_time = 2
+GPIO_UP = Button(5)
+GPIO_DOWN = Button(7)
+
+
+def play_story():
+    print("tell a story")
+
+
+def stop():
+    print("stop")
+
+
+def volume_up():
+    print("louder")
+
+
+def volume_down():
+    print("softer")
+
+
+presses = {
+    GPIO_PLAY: play_story,
+    GPIO_UP: volume_up,
+    GPIO_DOWN: volume_down,
+}
+
+holds = {
+    GPIO_PLAY: stop,
+}
+
+
+def wait_for_events():
+    while True:
+        for button, event in presses.items():
+            button.when_released = event
+        for button, event in holds.items():
+            button.when_held = event
+        pause()
+
+if __name__ == "__main__":
+    wait_for_events()
