3a8899c8e25ca6e072ba8b9f006dd7666518983a
Marino Linderhof <marino@mollie.com>
2017-10-02 23:22:17 +0200

    made some controls we need

--- sound_manager.py
+++ sound_manager.py
@@ -6,6 +6,19 @@ class SoundManager(object):
     def __init__(self):
         self.client = MPDClient()
         self.client.connect("localhost", 6600)
+        self.client.random(1)
+        self.client.setvol(50)
+        self.client.single(1)
 
     def status(self):
         return self.client.status()
+    
+    def pause(self):
+        self.client.pause()
+
+    def setVolume(self, volume):
+        self.client.setvol(volume)
+
+    def nextRandomSong(self):
+        self.client.next()
+    
