76d7ff7821aa935d34be733235dd15bde7746bb9
Marino Linderhof <marino@mollie.com>
2017-10-03 21:05:24 +0200

    new

--- sound_manager.py
+++ sound_manager.py
@@ -9,7 +9,8 @@ class SoundManager(object):
         self.client.setvol(50)
         self.client.single(1)
         self.client.add('/home/pi/sprookjes/music/Life Lessons with Mister T.mp3')
-
+        self.client.play()
+        
     def status(self):
         return self.client.status()
     
