4614f63a9f813b227a69033a527405dbed7f81c4
sprookje <you@example.com>
2018-03-20 20:35:52 +0000

    added wip

--- tell_a_tale/pin_events.py
+++ tell_a_tale/pin_events.py
@@ -1,32 +1,39 @@
-from gpiozero import Button
+from functools import partial
+from gpiozero import Button, RGBLED, Device
 from signal import pause
 from sound_manager import SoundManager
+from time import sleep
 
-GPIO_PLAY = Button(3)
-GPIO_UP = Button(5)
-GPIO_DOWN = Button(7)
+GPIO_PLAY = Button(21)
+RGB_LED = RGBLED(17,18,27)
 
-
-def play_story():
+def play_story(sound_manager):
     print("tell a story")
+    sound_manager.nextRandomSong()
 
-
-def stop():
+def stop(sound_manager):
     print("stop")
 
 
-def volume_up():
-    print("louder")
+def toggle_red_led(status=None):
+    # Because of NAND ON == OFF
+    if status == "on":
+        LED_RED.off()
+        return
+
+    if status == "off":
+        LED_RED.on()
+        return
+    
+    LED_RED.toggle();  
 
 
-def volume_down():
+def volume_down(sound_manager):
     print("softer")
 
 
 presses = {
     GPIO_PLAY: play_story,
-    GPIO_UP: volume_up,
-    GPIO_DOWN: volume_down,
 }
 
 holds = {
@@ -34,17 +41,43 @@ holds = {
 }
 
 
-def wait_for_events():
-    print("starting")
-    mine = SoundManager()
+def main():
+    # RGB_LED.blink( on_color=(0, 0, .1), off_color=(1, 0, 0), fade_in_time=2, fade_out_time=2, )
 
-    print('status:')
-    print (mine.status())
+    # blink(on_time=1, off_time=1, fade_in_time=0, fade_out_time=0, on_color=(1, 1, 1), off_color=(0, 0, 0), n=None, background=True)
+    # steps
+    # turn of red led and turn on yellow
+    # initiate sound manager
+    # green led on 
+    # wait for button
 
+    # sleep(10)
+    # LED_BLUE.off()
+    # LED_RED.off()
+
+    print("[MAIN] creating soundmanager")
+    mySoundManager = SoundManager()
+    print("[MAIN] status soundmanager")
+    print (mySoundManager.status())
+    RGB_LED.blink( on_color=(0, 0, .1), off_color=(1, 0, 0), fade_in_time=2, fade_out_time=2, )
+
+    print("[MAIN] Waiting for button press")
     while True:
         for button, event in presses.items():
-            button.when_pressed = event
+            button.when_pressed = partial(event, mySoundManager)
         pause()
 
-if __name__ == "__main__":
-    wait_for_events()
+# Define a destroy function for clean up everything after
+# the script finished 
+def destroy():
+    # LED_RED.off()
+    # Device.close()
+    print('Destroy')
+
+if __name__ == '__main__':
+    try:
+        main()
+    # When 'Ctrl+C' is pressed, the child program 
+    # destroy() will be  executed.
+    except KeyboardInterrupt:
+        destroy()
--- tell_a_tale/sound_manager.py
+++ tell_a_tale/sound_manager.py
@@ -1,28 +1,48 @@
-from mpd import MPDClient
-
+from persisten_client import PersistentMPDClient
+import asyncio
 
 class SoundManager(object):
-
-    def __init__(self, directory, host="localhost"):
-        self.client = MPDClient()
-        self.client.connect(host, 6600)
+    def __init__(self):
+        # Persistent MDP client it's wrapper for all actions
+        self.client = PersistentMPDClient(host="localhost", port=6600)
+        
+        # player
+        self.client.repeat(0)
         self.client.random(1)
         self.client.single(1)
+        self.client.consume(0)
+        #playlist
+        self.loadPlaylist('queue1')
+            
+    def loadPlaylist(self, playlist):
+        print('[LOAD PLAYLIST] clear')
+        self.client.clear()
+        print('[LOAD PLAYLIST] loading')
+        self.client.load('queue1')
+        print('[LOAD PLAYLIST] shuffle')
+        self.client.shuffle()
 
-    def status(self):
+    def status(self):                   
         return self.client.status()
-
-    def pause(self):
-        self.client.pause()
-
-    def setVolume(self, volume):
-        self.client.setvol(volume)
+    
+    async def poller(self):
+        print("Hello World!")
 
     def nextRandomSong(self):
-        self.client.next()
+        print('[NEXT RANDOM SONG]')
+        loop = asyncio.get_event_loop()
+        # Blocking call which returns when the hello_world() coroutine is done
+        loop.run_until_complete(self.poller())
+        loop.close()
 
-    def disconnect(self):
-        self.client.disconnect()
+        self.poller()
+        if self.status()['state'] == 'play':
+            print('status is play next!')
+            self.client.repeat(1)
+            self.client.next()
+            self.client.repeat(0)
+            return
+        self.client.play()
 
     def __del__(self):
-        self.disconnect()
+        self.client.disconnect()
